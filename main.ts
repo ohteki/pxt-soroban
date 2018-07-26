0a188aab3eab96b1464fd19605f21e2e9127587c
yusukeOteki <ut37186443@gmail.com>
2018-07-26 12:43:35 +0900

    refactored

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 //% weight=70 icon="\uf1ec" color=#00bbd7 block="そろばん"
 namespace soroban {
-		//% blockId=show_strings block="数を表示 %v"
-    export function showNumber(text: number = 0): void {
-        for (let x = 5 - text.toString().length; x < 5; x++)
-            (((d: number) => {
-                for (let y = 0; y < 5; y++)
-                    (y > 0 ? (d - 5 * (d < 5 ? 0 : 1) >= y) : (d > 4)) ? led.plot(x, y) : led.unplot(x, y)
-            })(parseInt(text.toString().substr(x - 5 + text.toString().length, 1))))
+    //% blockId=show_strings block="数を表示 %v"
+    export function showNumber(num: number = 0): void {
+        let str = num.toString()
+        for (let x = 5 - str.length; x < 5; x++) {
+            let d = parseInt(str.substr(x - 5 + str.length, 1))
+            for (let y = 0; y < 5; y++)
+                (y > 0 ? (d - 5 * (d < 5 ? 0 : 1) >= y) : (d > 4)) ? led.plot(x, y) : led.unplot(x, y)
+        }
     }
 }
