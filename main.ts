84c64dc081107ee8d6660dd1f0e3d40146c5f463
yusukeOteki <ut37186443@gmail.com>
2018-07-27 13:26:39 +0900

    locales

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=70 icon="\uf1ec" color=#00bbd7 block="soroban"
+//% weight=70 icon="\uf1ec" color=#00bbd7
 namespace soroban {
     //% blockId=show_number block="Show a number %num"
     export function showNumber(num: number = 0): void {
