1272f6065c375fc65b39c0ee3dc22f31e3b5d84f
yusukeOteki <ut37186443@gmail.com>
2018-07-27 12:32:13 +0900

    10, 100 order bag modified

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-//% weight=70 icon="\uf1ec" color=#00bbd7 block="そろばん"
+//% weight=70 icon="\uf1ec" color=#00bbd7 block="soroban"
 namespace soroban {
-    //% blockId=show_strings block="数を表示 %v"
+    //% blockId=show_number block="show a number %v"
     export function showNumber(num: number = 0): void {
         let str = num.toString()
-        for (let x = 5 - str.length; x < 5; x++) {
-            let d = parseInt(str.substr(x - 5 + str.length, 1))
+        for (let x = 0; x < 5; x++) {
+            let p = x - 5 + str.length
+            let d = p < 0 ? 0 : parseInt(str.substr(p, 1))
             for (let y = 0; y < 5; y++)
                 (y > 0 ? (d - 5 * (d < 5 ? 0 : 1) >= y) : (d > 4)) ? led.plot(x, y) : led.unplot(x, y)
         }
